42854fdda54d33ecebf07e4380f9f80c20391c5a
William Davies <bd1440@gmail.com>
2023-02-20 14:33:21 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function hide(element) {
+    element.remove();
+}
+
+function definitionLiked() {
+    alert('Ninja was liked');
+}
+
+function changeText(element, text) {
+    element.innerText = text;
+}
